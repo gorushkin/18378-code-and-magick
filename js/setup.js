4e297309a1042a27bc48fe156d6ba0e4a6748ab9
gorushkin <gorushkin@gmail.com>
2018-11-28 15:01:30 +0300

    Добавил пару функций

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,7 @@ var LAST_NAMES_LIST = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var NUMBER_OF_WIZARDS = 4;
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
 var HIDE_CLASS = 'hidden';
 
 var Selectors = {
@@ -15,14 +16,29 @@ var Selectors = {
   SIMILAR_WIZARD_TEMPLATE: '#similar-wizard-template',
   SIMILAR_LIST_WRAPPER: '.setup-similar',
   SIMILAR_LIST: '.setup-similar-list',
-  SIMILAR_LIST_ITEM: '.setup-similar-item'
+  SIMILAR_LIST_ITEM: '.setup-similar-item',
+  SETUP_OPEN_ICON: '.setup-open',
+  SETUP_CLOSE_BUTTON: '.setup-close'
 };
 
 
 var setupPopup = document.querySelector(Selectors.SETUP_POPUP);
-setupPopup.classList.remove(HIDE_CLASS);
+// setupPopup.classList.remove(HIDE_CLASS);
 var similarListElement = setupPopup.querySelector(Selectors.SIMILAR_LIST);
+var setupCloseButton = setupPopup.querySelector(Selectors.SETUP_CLOSE_BUTTON);
+var similarOpenIcon = document.querySelector(Selectors.SETUP_OPEN_ICON);
 var similarWizardTemplate = document.querySelector(Selectors.SIMILAR_WIZARD_TEMPLATE).content.querySelector(Selectors.SIMILAR_LIST_ITEM);
+
+
+similarOpenIcon.addEventListener('click', function () {
+  setupPopup.classList.remove(HIDE_CLASS);
+});
+
+setupCloseButton.addEventListener('click', function () {
+  setupPopup.classList.add(HIDE_CLASS);
+});
+
+
 
 var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
