3e8138811ee2747706eeeb392941b7981a3f6813
gorushkin <gorushkin@users.noreply.github.com>
2018-12-03 23:50:32 +0300

    исправил багу с перетаскиванием окна

--- js/dialog.js
+++ js/dialog.js
@@ -5,6 +5,11 @@
   window.setup.setupUserPic.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
+    var windowWidth = document.documentElement.clientWidth;
+    var windowHeight = document.documentElement.clientHeight;
+    var setupPopupHeight = window.setup.setupPopup.offsetHeight;
+    var setupPopupWidth = window.setup.setupPopup.offsetWidth;
+
     var startCoords = {
       x: evt.clientX,
       y: evt.clientY
@@ -36,6 +41,17 @@
 
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
+
+      if (window.setup.setupPopup.offsetLeft < setupPopupWidth / 2) {
+        window.setup.setupPopup.style.left = setupPopupWidth / 2 + 'px';
+      } else if (window.setup.setupPopup.offsetLeft > windowWidth - setupPopupWidth / 2) {
+        window.setup.setupPopup.style.left = (windowWidth - setupPopupWidth / 2) + 'px';
+      }
+      if (window.setup.setupPopup.offsetTop < 0) {
+        window.setup.setupPopup.style.top = 0 + 'px';
+      } else if (window.setup.setupPopup.offsetTop > windowHeight - setupPopupHeight) {
+        window.setup.setupPopup.style.top = (windowHeight - setupPopupHeight) + 'px';
+      }
 
       if (dragged) {
         var onClickPreventDefault = function (clickEvt) {
